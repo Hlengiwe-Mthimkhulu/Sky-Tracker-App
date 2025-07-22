5b737d0b4ec28196cd109e2498a05d69d8528955
Hlengiwe-Mthimkhulu <hlengiwenorah29@gmail.com>
2025-07-23 00:11:07 +0200

    updated the basic search engine

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 function launchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  searchInput.innerHTML("search-input");
+  let searchElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city-input");
+  cityElement.innerHTML = searchElement.value;
 }
 
 let searchForm = document.querySelector("#search-form");
