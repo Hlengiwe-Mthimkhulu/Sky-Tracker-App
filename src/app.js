a025fd021c43595f6dc2208a51726bbf8c40a7a0
Hlengiwe-Mthimkhulu <hlengiwenorah29@gmail.com>
2025-07-28 23:09:23 +0200

    Inject the forecast HTML from JavaScript

--- src/app.js
+++ src/app.js
@@ -57,5 +57,30 @@ function launchSubmit(event) {
   searchCity(searchElement.value);
 }
 
+function displayForecast() {
+  let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>20°</strong>
+              </div>
+              <div class="weather-forecast-temperature">10°</div>
+            </div>
+          </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", launchSubmit);
+
+displayForecast();
